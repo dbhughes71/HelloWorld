033cb2ba66599139bb2c7ffb226b7d8579a03a66
David Hughes <davidhughes@iMac.local>
2016-06-04 18:00:07 +0800

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+/**
+ * Created by davidhughes on 4/06/16.
+ *
+ * This project will be used as a vehicle to explore IntelliJ IDE and GitHub
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
