fdb6ff8c3bad3d8455a9597bc7216e234d2e5cd9
davidhughes <dbhughes71@gmail.com>
2016-06-11 11:24:51 +0800

    Added sample code to illustrate multidimensional arrays with and without differing lengths on the secondary dimensions.

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -79,6 +79,38 @@ public class HelloWorld {
             int month_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30};
             System.out.println("April has " + month_days[3] + " days.");
         }
+
+        //Multidimensional arrays
+        {
+            int twoD[][] = new int[4][5];
+            int k = 0;
+            for (int i=0; i<4; i++){
+                for (int j=0; j<5; j++) {
+                    twoD[i][j]=k;
+                    System.out.print(k + " ");
+                    k++;
+                }
+                System.out.println();
+            }
+        }
+
+        //Multidimensional arrays with differing size secondary dimensions
+        {
+            int twoD[][] = new int[4][];
+            twoD[0] = new int[1];
+            twoD[1] = new int[2];
+            twoD[2] = new int[3];
+            twoD[3] = new int[4];
+            int k=0;
+            for (int i=0; i<4; i++){
+                for (int j=0; j<i+1; j++){
+                    twoD[i][j] = k;
+                    System.out.print(k + " ");
+                    k++;
+                }
+                System.out.println();
+            }
+        }
     }
 
 }
