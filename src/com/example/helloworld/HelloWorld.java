7a933875d123793495bd05777652dee750866d48
davidhughes <dbhughes71@gmail.com>
2016-06-11 10:45:54 +0800

    Added sample code to illustrate type promotion rules.

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -60,5 +60,20 @@ public class HelloWorld {
             //y = 100;
             System.out.println("x is " + x);
         }
+
+        //Demonstrate type promotion rules
+        {
+            byte b = 42;
+            char c = 'a';
+            short s = 1024;
+            int i = 50000;
+            float f = 5.67f;
+            double d = 0.1234;
+            double result = (f * b) + (i / c) - (d * s);
+            System.out.println((f * b) + " + " + (i / c) + " - " + (d * s));
+            System.out.println("result = " + result);
+        }
     }
+
 }
+
