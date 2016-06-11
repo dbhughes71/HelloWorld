286f4a21e109e4bcf4e72274c4df8d7733977b79
davidhughes <dbhughes71@gmail.com>
2016-06-11 10:32:08 +0800

    Added sample code to illustrate how code blocks constrain variable scope.

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -11,34 +11,54 @@ public class HelloWorld {
         System.out.println("Hello, World!");
 
         //If statements sample
-        int x,y;
-        x = 10;
-        y = 20;
-        if (x<y) System.out.println("x is less than y");
-        x=x*2;
-        if (x==y) System.out.println("x now equal to y");
-        x=x*2;
-        if (x>y) System.out.println("x is greater than y");
-        if (x==y) System.out.println("x now equal to y");
+        {
+            int x, y;
+            x = 10;
+            y = 20;
+            if (x < y) System.out.println("x is less than y");
+            x = x * 2;
+            if (x == y) System.out.println("x now equal to y");
+            x = x * 2;
+            if (x > y) System.out.println("x is greater than y");
+            if (x == y) System.out.println("x now equal to y");
+        }
 
         //For loop sample code
-        for(x=0;x<10;x=x+1)
-            System.out.println("This is x: " + x);
+        {
 
+            for (int x = 0; x < 10; x = x + 1)
+                System.out.println("This is x: " + x);
+        }
 
         //Compute the area of a circle
-        double pi, r, a;
-        r = 10.8;
-        pi = 3.1416;
-        a = pi * r * r;
-        System.out.println("Area of the circle is " +a);
-
+        {
+            double pi, r, a;
+            r = 10.8;
+            pi = 3.1416;
+            a = pi * r * r;
+            System.out.println("Area of the circle is " + a);
+        }
 
         //Demonstrate char data type.
-        char ch1, ch2;
-        ch1 = 88;  //code for X
-        ch2 = 'Y';
-        System.out.println("ch1 and ch2: ");
-        System.out.println(ch1 + " " + ch2);
+        {
+            char ch1, ch2;
+            ch1 = 88;  //code for X
+            ch2 = 'Y';
+            System.out.println("ch1 and ch2: ");
+            System.out.println(ch1 + " " + ch2);
+        }
+
+        //Demonstrate block scope
+        {
+            int x;
+            x = 10;
+            if (x == 10) {
+                int y = 20;
+                System.out.println("x and y:" + x + " " + y);
+                x = y * 2;
+            }
+            //y = 100;
+            System.out.println("x is " + x);
+        }
     }
 }
